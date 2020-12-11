643145281a586d840324ee5cc23ac1102a181204
venkatpattem <pvkrishnaiah1991@gmail.com>
2020-12-11 16:52:07 +0530

    prospective buyesrs and vendors

---
+++ src/components/Core/prospective buyer/ProspectiveBuyerAdd.js
@@ -0,0 +1,287 @@
+import React, { Component } from 'react';
+import ContentWrapper from '../../Layout/ContentWrapper';
+import { connect } from 'react-redux';
+import swal from 'sweetalert';
+import axios from 'axios';
+
+import { server_url, context_path, } from '../../Common/constants';
+import { Button, TextField,  } from '@material-ui/core';
+// import AutoSuggest from '../../Common/AutoSuggest';
+
+import 'react-datetime/css/react-datetime.css';
+// import MomentUtils from '@date-io/moment';
+// import {
+//     DatePicker,
+//     MuiPickersUtilsProvider,
+// } from '@material-ui/pickers';
+// import Event from '@material-ui/icons/Event';
+
+
+import FormValidator from '../../Forms/FormValidator';
+import {  Form } from 'reactstrap';
+
+// import Radio from '@material-ui/core/Radio';
+// import RadioGroup from '@material-ui/core/RadioGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+
+// const json2csv = require('json2csv').parse;
+
+class ProspectiveBuyerAdd extends Component {
+
+    state = {
+        editFlag: false,
+
+        formWizard: {
+            globalErrors: [],
+            msg: '',
+            errors: {},
+            obj: {
+                id: 0,
+                name: '',
+                email:'',
+                address: '',
+                phone:'',
+                other: '',
+                contactName:''
+            }
+        }
+    }
+
+    loadData() {
+        axios.get(server_url + context_path + "api/" + this.props.baseUrl + "/" + this.state.formWizard.obj.id + '?projection=template_edit')
+            .then(res => {
+                var formWizard = this.state.formWizard;
+                formWizard.obj = res.data;
+                this.setState({ formWizard });
+            });
+    }
+    createNewObj() {
+        var formWizard = {
+            globalErrors: [],
+            msg: '',
+            errors: {},
+            obj: {
+                name: '',
+                email:'',
+                address: '',
+                phone:'',
+                other: '',
+                contactName:''
+            }
+        }
+
+        this.setState({ formWizard });
+    }
+
+    updateObj(id) {
+        var formWizard = this.state.formWizard;
+        formWizard.obj.id = id;
+        formWizard.editFlag = true;
+
+        this.setState({ formWizard }, this.loadData);
+    }
+
+    setField(field, e, noValidate) {
+        var formWizard = this.state.formWizard;
+
+        var input = e.target;
+        formWizard.obj[field] = e.target.value;
+        this.setState({ formWizard });
+
+        if(!noValidate) {
+            const result = FormValidator.validate(input);
+            formWizard.errors[input.name] = result;
+            this.setState({
+                formWizard
+            });
+        }
+    }
+
+    setSelectField(field, e) {
+        this.setField(field, e, true);
+    }
+
+    setDateField(field, e) {
+        var formWizard = this.state.formWizard;
+
+        if(e) {
+            formWizard.obj[field] = e.format();
+        } else {
+            formWizard.obj[field] = null;
+        }
+
+        this.setState({ formWizard });
+    }
+
+    setAutoSuggest(field, val) {
+        var formWizard = this.state.formWizard;
+        formWizard.obj[field] = val;
+        formWizard['selected' + field] = val;
+        this.setState({ formWizard });
+    }
+
+    checkForError() {
+        // const form = this.formWizardRef;
+
+        const tabPane = document.getElementById('saveForm');
+        const inputs = [].slice.call(tabPane.querySelectorAll('input,select'));
+        const { errors, hasError } = FormValidator.bulkValidate(inputs);
+        var formWizard = this.state.formWizard;
+        formWizard.errors = errors;
+        this.setState({ formWizard });
+        console.log(errors);
+
+        return hasError;
+    }
+
+    saveDetails() {
+        var hasError = this.checkForError();
+        if (!hasError) {
+        var newObj = this.state.formWizard.obj;
+        newObj['categories'] = "";
+        var promise = undefined;
+
+        if (!this.state.editFlag) {
+            promise = axios.post(server_url + context_path + "api/" + this.props.baseUrl, newObj)
+        } else {
+            promise = axios.patch(server_url + context_path + "api/" + this.props.baseUrl + "/" + this.state.formWizard.obj.id, newObj)
+        }
+
+        promise.then(res => {
+            var formw = this.state.formWizard;
+            formw.obj.id = res.data.id;
+            formw.msg = 'successfully Saved';
+
+            this.props.onSave(res.data.id);
+        }).finally(() => {
+            this.setState({ loading: false });
+        }).catch(err => {
+            // this.toggleTab(0);
+            //this.setState({ addError: err.response.data.globalErrors[0] });
+            var formWizard = this.state.formWizard;
+            formWizard.globalErrors = [];
+            if (err.response.data.globalErrors) {
+                err.response.data.fieldError.forEach(e => {
+                    formWizard.globalErrors.push(e);
+                });
+            }
+
+            var errors = {};
+            if (err.response.data.fieldError) {
+                err.response.data.fieldError.forEach(e => {
+                    if (errors[e.field]) {
+                        errors[e.field].push(e.errorMessage);
+                    } else {
+                        errors[e.field] = [];
+                        errors[e.field].push(e.errorMessage);
+                    }
+                });
+            }
+            formWizard.errors = errors;
+            this.setState({ formWizard });
+            swal("Unable to Save!", "Please resolve the errors", "error");
+        })
+    }
+        return true;
+    }
+    
+    componentWillUnmount() {
+        this.props.onRef(undefined);
+    }
+
+    componentDidMount() {
+        this.props.onRef(this);
+        this.setState({ loding: false })
+    }
+
+    render() {
+        const errors = this.state.formWizard.errors;
+
+        return (
+            <ContentWrapper>
+                <Form className="form-horizontal" innerRef={this.formRef} name="formWizard" id="saveForm">
+
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+
+                            <fieldset>
+                                <TextField
+                                    type="text"
+                                    label="Name"
+                                    name="name"
+                                    required={true}
+                                    fullWidth={true}
+                                    readOnly={true}
+                                    inputProps={{maxLength: 30, "data-validate": '[{ "key":"minlen","param":"3"},{"key":"maxlen","param":"30"}]' }}
+                                    value={this.state.formWizard.obj.name}
+                                    onChange={e => this.setField('name', e)} />
+                            </fieldset>
+                            <fieldset>
+                                <TextField
+                                    type="text"
+                                    label="Contact Name"
+                                    name="contactName"
+                                    required={true}
+                                    fullWidth={true}
+                                    readOnly={true}
+                                    inputProps={{maxLength: 30, "data-validate": '[{ "key":"minlen","param":"3"},{"key":"maxlen","param":"30"}]' }}
+                                    value={this.state.formWizard.obj.contactName}
+                                    onChange={e => this.setField('contactName', e)} />
+                            </fieldset>
+                            <fieldset>
+                                <TextField
+                                    type="text"
+                                    name="Phone"
+                                    label="Phone"
+                                    required={true}
+                                    fullWidth={true}
+                                    inputProps={{ maxLength: 13 }}
+                                    value={this.state.formWizard.obj.phone}
+                                    onChange={e => this.setField('phone', e)} />
+                            </fieldset>
+                            <fieldset>
+                                <TextField
+                                    type="text"
+                                    name="email"
+                                    label="Email"
+                                    required={true}
+                                    fullWidth={true}
+                                    inputProps={{ minLength: 5, maxLength: 30, "data-validate": '[{ "key":"email"}]' }}
+                                    helperText={errors?.email?.length > 0 ? errors?.email[0]?.msg : ""}
+                                    error={errors?.email?.length > 0}
+                                    value={this.state.formWizard.obj.email}
+                                    onChange={e => this.setField('email', e)} />
+                            </fieldset>
+                            <fieldset>
+                                        <TextField
+                                            name="other"
+                                            type="text"
+                                            label="Others"
+                                             
+                                            fullWidth={true}
+                                            inputProps={{ minLength: 0, maxLength: 300 }}
+                                            value={this.state.formWizard.obj.other}
+                                            onChange={e => this.setField('other', e)} />
+                                    </fieldset>
+
+                            <div className="text-center">
+                                <Button variant="contained" color="secondary" onClick={e => this.props.onCancel()}>Cancel</Button>
+                                <Button variant="contained" color="primary" onClick={e => this.saveDetails()}>Save & Continue</Button>
+                            </div>
+                        </div>
+                    </div>
+                </Form>
+            </ContentWrapper>)
+    }
+}
+
+const mapStateToProps = state => ({
+    settings: state.settings,
+    user: state.login.userObj
+})
+
+export default connect(
+    mapStateToProps
+)(ProspectiveBuyerAdd);
