8721f5815ec9a270bc196daf47863bc58b2b9146
rakesh <pencapdevelopment@gmail.com>
2021-01-12 18:35:41 +0530

    sales enquiry add.js if condition added

--- src/components/Core/SalesEnquiry/Add.js
+++ src/components/Core/SalesEnquiry/Add.js
@@ -106,7 +106,9 @@ class Add extends Component {
                 this.setState({ formWizard }, o => {
                     if (res.data.products) {
                         res.data.products.forEach((p, idx) => {
-                            this.productASRef[idx].setInitialField(formWizard.selectedProducts[idx]);
+                            if(this.productASRef[idx]){
+                                this.productASRef[idx].setInitialField(formWizard.selectedProducts[idx]);
+                            }
                         });
                     }
                 });
@@ -265,7 +267,8 @@ class Add extends Component {
         var products = formWizard.obj.products;
 
         if (products[i].id) {
-            products[i].delete = true;
+            
+            [i].delete = true;
         } else {
             products.splice(i, 1);
             formWizard.selectedProducts.splice(i, 1);
