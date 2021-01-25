730ea6a8ab72dd8a15154e18888f04667468b482
naveenbodatrevista <naveenboda@trevistainnovations.com>
2021-01-25 12:21:08 +0530

    rename assign users

--- src/components/Core/Common/AssignUsers.js
+++ src/components/Core/Common/AssignUsers.js
@@ -2,38 +2,38 @@ import axios from 'axios';
 import swal from 'sweetalert';
 import { context_path, server_url } from '../../Common/constants';
 
-export function saveUsers(baseUrl, objId, products, callBack) {
-    var productsUrl = server_url + context_path + "api/" + baseUrl + '-user/';
+export function saveUsers(baseUrl, objId, users, callBack) {
+    var usersUrl = server_url + context_path + "api/" + baseUrl + '-user/';
    
-    products.forEach((prod, idx) => {
-        console.log(prod);
-        if(prod.delete) {
-            axios.delete(productsUrl + prod.id)
+    users.forEach((user, idx) => {
+        console.log(user);
+        if(user.delete) {
+            axios.delete(usersUrl + user.id)
                 .then(res => {
 
                 }).catch(err => {
                     swal("Unable to Delete!", err.response.data.globalErrors[0], "error");
                 })
-        } else if(prod.id || prod.updated) {
+        } else if(user.id || user.updated) {
             console.log('Testsss 123456');
-            prod.reference = '/' + baseUrl + '/' + objId;
-            prod.user = '/products/' + prod.id;
+            user.reference = '/' + baseUrl + '/' + objId;
+            user.user = '/users/' + user.id;
              
             var promise = undefined;
-            if (prod.id) {
-                promise = axios.post(productsUrl, prod);
+            if (user.id) {
+                promise = axios.post(usersUrl, user);
             } else {
-                promise = axios.patch(productsUrl + prod.id, prod);
+                promise = axios.patch(usersUrl + user.id, user);
             }
 
             promise.then(res => {
-                prod.id = res.data.id;
+                user.id = res.data.id;
             }).catch(err => {
                 swal("Unable to Save!", "Please resolve the errors", "error");
             })
         }
 
-        if(idx === products.length - 1) {
+        if(idx === users.length - 1) {
             setTimeout(() => {
                 if(callBack) {
                     callBack();
